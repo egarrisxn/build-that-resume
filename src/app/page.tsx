b6b1dc62a0d6b0a262427ae68cf52888a224ed53
EGARRISXN <egarrisxn@gmail.com>
2024-05-22 15:53:39 -0400

    tweaked some things.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import Link from "next/link";
 
+const linkAttr = 'underline underline-offset-2 text-blue-600 hover:text-blue-300'
+
 export default function ResumePage() {
   return (
-    <div className="text-center pt-24">
-      <Link href="/resume">Build That Resume Now!</Link>
+    <div className="text-center 2xl:text-xl pt-24">
+      <span className="font-bold">Build That Resume!</span>
+      <br/><br/>
+      Click <Link href="/resume" className={`${linkAttr}`}>here</Link> for an example.
+      <br/>
+      Click <Link href='/form' className={`${linkAttr}`}>here</Link> for the template.
+      <br/>
+      Click <a href='https://www.youtube.com/watch?v=o-YBDTqX_ZU' target="_blank" rel="noopener noreferrer" className={`${linkAttr}`}>here</a> for a fun video.
     </div>
   );
 }
